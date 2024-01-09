d8843b598d11c17e200558a326257ce68d01e81f
Erencan Güneş <erencan.gunes@figensoft.com>
2024-01-09 16:17:31 +0300

    ..

---
+++ OpenTelemetryConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observability_With_Opentelemetry
+{
+    internal class OpenTelemetryConstants
+    {
+
+        internal const string ServiceName = "CompanyName.ECommerce.ConsoleApp"; // "CompanyName.ECommerce.ComponentApp"
+        internal const string ServiceVersion = "1.0.0";
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Observability_With_Opentelemetry;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 Console.WriteLine("Hello, World!");
+
+var traveProvider = Sdk.CreateTracerProviderBuilder()
+                        .ConfigureResource(configure =>
+                        {
+                            configure.AddService(OpenTelemetryConstants.ServiceName, OpenTelemetryConstants.ServiceVersion)
+                                        .AddAttributes(new List<KeyValuePair<string, object>>()
+                                        {
+                                        new KeyValuePair<string, object>("host.machine", Environment.MachineName),
+                                        new KeyValuePair<string, object>("host.os", Environment.OSVersion.VersionString),
+                                        new KeyValuePair<string, object>("host.version", "dev")
+                                        });
+
+                        }).Build();
+
+traveProvider.
